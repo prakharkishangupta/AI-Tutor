66e9e402f20b4e7f3267de0880fecf3da181eed3
Prakhar Kumar Gupta <prakharkumargupta@Prakhars-MacBook-Air.local>
2026-02-10 17:21:49 +0530

    importing dependency

--- Frontend/src/components/SubtitleItem.jsx
+++ Frontend/src/components/SubtitleItem.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import api from "../api/axios";
+import { useState } from "react";
 
 const SubtitleItem = ({ subtitle, courseId, subtopicId, subtopicTitle }) => {
   const navigate = useNavigate();
